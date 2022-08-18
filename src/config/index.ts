6c2400d70af8e5fb5c0b5279f97871ffa4071680
Rasel5267 <mahadiul09@.com>
2022-08-18 06:03:02 +0600

    fixed config file problem over deploing on heroku

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+export const APP_SECRET = 'w59ChnxwqSms0WRYl&6eq9TOvNgdWdc!55bG#Rdq49@JbBUu9w'
+export const MONGO_URI = 'mongodb+srv://admin:admin@cluster0.hk3oj.mongodb.net/faco?retryWrites=true&w=majority'
+
+export const PORT = process.env.PORT || 8000
+export const SENDER_EMAIL = 'mahadiul09@gmail.com'
+export const PASSWORD = 'lmzythomkfyyexwm'
+
+
+export const CLOUDINARY_NAME = 'mahadiul5267'
+export const CLOUDINARY_API_KEY = '111894769879832'
+export const CLOUDINARY_API_SECRET = '_I2OxFpfwMfG3dUWQ-fmbyRV69k'
+
